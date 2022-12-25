7ede441cbd8d6cca1e5de4a3e608b766b7e51b77
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2022-12-25 03:02:23 -0600

    Create A The number of positions.cpp

---
+++ Codeforces/A The number of positions.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define f firts
+#define s second 
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+#define pot(a) ((a)*(a))
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+lli a, b, n, r(0);
+int main () { _
+    cin >> n >> a >> b;
+    r = min(n - a, b + 1);
+    cout << r << ENDL;
+    return 0;  
+}
