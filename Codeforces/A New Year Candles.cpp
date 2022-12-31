4f18e7de3a4e9d4a4e1fbf545e184f7bbe635348
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2022-12-30 21:01:01 -0600

    Create A New Year Candles.cpp

---
+++ Codeforces/A New Year Candles.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second 
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+#define pot(a) ((a)*(a))
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+lli a(0), b(0), ans(0), ab(0);
+int main() { _
+    cin >> a >> b;
+    while (a >= b) {
+        ans += b;
+        a -= b - 1;
+    }
+    ans += a;
+    cout << ans << ENDL;
+    return 0; 
+}
