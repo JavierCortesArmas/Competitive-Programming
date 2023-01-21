665952e1f6d21b46606ce957c8c9cba7a5ac08d0
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2023-01-21 05:42:10 -0600

    Create A Maximum GCD.cpp

---
+++ Codeforces/A Maximum GCD.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second 
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define l '\n'
+#define c cout    
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+lli n, a, b;
+int main() { _ 
+  cin >> n;
+  fore (i, 0, n) {
+    cin >> a;
+    c << a / 2 << l;
+  }
+  return 0;
+}
+
+
+
+
