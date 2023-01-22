4eb23a9f53694a095bd2d4b4a5b9920cc851d00a
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2023-01-22 16:30:56 -0600

    Create A Holiday Of Equality.cpp

---
+++ Codeforces/#include <bits/A Holiday Of Equality.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second 
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define l '\n'
+#define c cout    
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+lli n, cnt(0); 
+int main() { _ 
+  cin >> n;
+  lli a[n];
+  lli a2(0);
+  fore (i, 0, n) {
+    cin >> a[i];
+  }
+  sort(a, a + n);
+  fore (i, 0, n) {
+    cnt += a[n - 1] - a[i];
+  }
+  c << cnt << l;
+  return 0;
+}
+
+
+
+
+
