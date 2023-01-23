6474acfc6ec1b851b02af171f0ffc41206d7658f
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2023-01-22 20:46:20 -0600

    Create A Bit++.cpp

---
+++ Codeforces/A Bit++.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second 
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define l '\n'
+#define c cout    
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+lli n, x, x2, r;
+int main() { _ 
+  cin >> n;
+  string bit;
+  fore (i, 0, n) {
+    cin >> bit;
+    if (bit[0] == '+' && bit[1] == '+' && bit[2] == 'X' || bit[0] == 'X' && bit[1] == '+' && bit[2] == '+') {
+      x++;
+    }
+    else if (bit[0] == '-' && bit[1] == '-' && bit[2] == 'X' || bit[0] == 'X' && bit[1] == '-' && bit[2] == '-') {
+      x2--;
+    }
+  }
+  r = x+=x2;
+  c << r << l;
+  return 0;
+}
+
+
+
+
+
