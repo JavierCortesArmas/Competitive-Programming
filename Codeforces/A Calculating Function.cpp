9f9678d1327925cf88fa5b8c29b754bd49114ea2
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2023-01-29 00:35:23 -0600

    Create A Calculating Function.cpp

---
+++ Codeforces/A Calculating Function.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second 
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define l '\n'
+#define c cout    
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+lli n(1e15), Fn(0), ans(0);
+int main () { _
+  cin >> n;
+  if (n % 2 == 0) { ans = n / 2; }
+  else { ans = ((n + 1) / 2) * (-1); }
+  c << ans << l;
+  return 0;
+}
