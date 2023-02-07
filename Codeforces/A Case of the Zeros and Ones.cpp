4c8b641ee8726f1a9930ceaf7f59200e8f40f8b7
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2023-02-06 23:48:26 -0600

    Create A Case of the Zeros and Ones.cpp

---
+++ Codeforces/A Case of the Zeros and Ones.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second 
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define l '\n'
+#define c cout    
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+lli n0, n1, ans(0), n; char s;
+int main () { _ 
+  cin >> n;
+  fore (i, 0, n) {
+    cin >> s;
+    if (s == '0') { n0++; }
+    else if (s == '1') { n1++; }
+  }
+  c << abs(n0-n1) << l;
+  return 0;
+}
