b57ca34e9da375f4f4a13ccdca42d67dfcb1bfb2
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2023-02-14 00:52:37 -0600

    Create A Wrong Subtraction.cpp

---
+++ Codeforces/A Wrong Subtraction.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second 
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define l '\n'
+#define c cout    
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+lli n, k;
+int main () { _
+  cin >> n >> k;
+  fore (i, 0, k) {
+    if (n % 10 == 0) n /= 10;
+    else n--; 
+  }
+  c << n << l;
+  return 0;
+}
