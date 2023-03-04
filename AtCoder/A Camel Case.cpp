cb05d0935c206c6f59e74f634a01a9907ada1287
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2023-03-03 21:59:04 -0600

    Create A Camel Case.cpp

---
+++ AtCoder/A Camel Case.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second 
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define l '\n'
+#define c cout    
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+string n; lli cnt;
+int main () { _
+  cin >> n;
+  string a;
+  fore (i, 0, n.length()) {
+    if (n[i] >= 'A' && n[i] <= 'Z') {
+      c << i + 1 << l;
+      break;
+    }
+  }
+  return 0;
+}
+
