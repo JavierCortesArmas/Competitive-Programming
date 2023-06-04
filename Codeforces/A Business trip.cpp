4d1cb43a84e68486d357dfb538fcfc5849bc5ad1
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2023-06-04 08:04:50 -0600

    Create A Business trip.cpp

---
+++ Codeforces/A Business trip.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second 
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size()), 
+#define l '\n'
+#define c cout    
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+
+void Algorithm(lli array[], lli n) {
+  for(lli i = 0; i < n - 1; i++) {
+    lli maxIndex = i;
+    for(lli j = i + 1; j < n; j++) {
+      if(array[j] > array[maxIndex]) {
+        maxIndex = j;
+      }
+    }
+    if(maxIndex != i) {
+      lli temp = array[i];
+      array[i] = array[maxIndex];
+      array[maxIndex] = temp;
+    }
+  }
+}
+
+int main() { _
+  lli array[12], sum(0), k;
+  cin >> k;
+  fore(i, 0, 12) {
+    cin >> array[i];
+    sum += array[i];
+  }
+  if(sum < k) {
+    c << -1 << l;
+  }
+  else {
+    Algorithm(array, 12);
+    lli ans(0), cnt(0);
+    while(ans < k) {
+      ans += array[cnt];
+      cnt += 1;
+    }
+    c << cnt << l;
+  }
+  return 0;
+}
